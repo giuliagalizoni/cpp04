a7abd246ff471598948c213bd4df7f44d56d3251
Giulia Galizoni <giuliagalizoni@gmail.com>
2025-11-22 12:33:30 +0100

    unswitch cat and wrongcat

--- ex00/src/Cat.cpp
+++ ex00/src/Cat.cpp
@@ -1,37 +1,37 @@
-#include "WrongCat.hpp"
+#include "Cat.hpp"
 
-#include "WrongAnimal.hpp"
+#include "Animal.hpp"
 
 // Default constructor
-WrongCat::WrongCat() : WrongAnimal()
+Cat::Cat() : Animal()
 {
 	type = "Cat";
 	std::cout << type << " default constructor called" << std::endl;
 }
 
 // Copy constructor
-WrongCat::WrongCat(const WrongCat& other) : WrongAnimal(other)
+Cat::Cat(const Cat& other) : Animal(other)
 {
-	std::cout << "WrongCat copy constructor called" << std::endl;
+	std::cout << "Cat copy constructor called" << std::endl;
 }
 
 // Copy assignment operator
-WrongCat& WrongCat::operator=(const WrongCat& other)
+Cat& Cat::operator=(const Cat& other)
 {
-	std::cout << "WrongCat copy assignment operator called" << std::endl;
+	std::cout << "Cat copy assignment operator called" << std::endl;
 	if (this != &other)
-		WrongAnimal::operator=(other);
+		Animal::operator=(other);
 	return *this;
 }
 
 // Destructor
-WrongCat::~WrongCat()
+Cat::~Cat()
 {
-	std::cout << "WrongCat destructor called" << std::endl;
+	std::cout << "Cat destructor called" << std::endl;
 }
 
 // Public methods implementation
-void WrongCat::makeSound() const
+void Cat::makeSound() const
 {
 	std::cout << "miau" << std::endl;
 }
